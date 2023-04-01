c78ec0f851d8414c18f63875c852c730856ffdc7
ostopenchuk <username@eliteit.org>
2023-04-01 22:44:35 +0300

    lesson 34 - 8 minutes

--- www/core/admin/controllers/BaseAdmin.php
+++ www/core/admin/controllers/BaseAdmin.php
@@ -13,15 +13,16 @@ abstract class BaseAdmin extends BaseController
     protected $model;
     protected $table;
     protected $data;
-
+    protected $title;
     protected $columns;
     protected $menu;
 
     protected function inputData()
     {
+        $test = Settings::instance()->arrayMergeRecursive();
 
         $this->init(true);
-
+ 
         $this->title = 'engine';
 
         if (!$this->model) $this->model = Model::instance();
@@ -61,84 +62,6 @@ abstract class BaseAdmin extends BaseController
         if (!$this->columns) new RouteException('Не найдены поля в таблице - ' . $this->table, 2);
     }
 
-    protected function createData($arr = [], $add = true)
-    {
-
-        $fields = [];
-        $order = [];
-        $order_direction = [];
-
-        if ($add) {
-
-            if (!$this->columns['id_row']) return $this->data = [];
-
-            $fields[] = $this->columns['id_row'] . ' as id';
-            if ($this->columns['name']) $fields['name'] = 'name';
-            if ($this->columns['img']) $fields['img'] = 'img';
-
-            if (count($fields) < 3) {
-                foreach ($this->columns as $key => $item) {
-                    if (!$this->fields['name'] && strpos($key, 'name') !== false) {
-                        $fields['name'] = $key . ' as name';
-                    }
-                    if (!$this->fields['img'] && strpos($key, 'img') == 0) {
-                        $fields['img'] = $key . ' as img';
-                    }
-                }
-            }
-
-            if ($arr['fields']) {
-                if (is_array($arr['fields'])) {
-                    $fields = Settings::instance()->arrayMergeRecursive($fields, $arr['fields']);
-                } else {
-                    $fields[] = $arr['fields'];
-                }
-            }
-
-            if ($this->columns['parent_id']) {
-                if (!in_array('parent_id', $fields)) $fields[] = 'parent_id';
-                $order[] = 'parent_id';
-            }
-
-            if ($this->columns['manu_position']) $order[] = 'menu_position';
-            elseif ($this->columns['date']) {
-                if ($order) $order_direction = ['ASC', 'DESC'];
-                else $order_direction[] = ['DESC'];
-
-                $order[] = 'date';
-            }
-
-            if ($arr['order']) {
-                if (is_array($arr['order'])) {
-                    $order = Settings::instance()->arrayMergeRecursive($order, $arr['order']);
-                } else {
-                    $order[] = $arr['order'];
-                }
-            }
-            if ($arr['order_direction']) {
-                if (is_array($arr['order_direction'])) {
-                    $order_direction = Settings::instance()->arrayMergeRecursive($order_direction, $arr['order_direction']);
-                } else {
-                    $order_direction[] = $arr['order_direction'];
-                }
-                $order_direction = Settings::instance()->arrayMergeRecursive($order_direction, $arr['order_direction']);
-            }
-        } else {
-
-            if (!$arr) return $this->data = [];
-
-            $fields = $arr['fields'];
-            $order = $arr['order'];
-            $order_direction = ['order_direction'];
-        }
-
-        $this->data = $this->model->get($this->table, [
-            'fields' => $fields,
-            'order' => $order,
-            'order_direction' => $order_direction
-        ]);
-    }
-
     protected function expansion($args = [])
     {
 
@@ -160,4 +83,6 @@ abstract class BaseAdmin extends BaseController
             $res = $exp->expansion($args);
         }
     }
+
+    
 }
--- www/core/admin/controllers/ShowController.php
+++ www/core/admin/controllers/ShowController.php
@@ -2,6 +2,8 @@
 
 namespace core\admin\controllers;
 
+use core\base\settings\Settings;
+
 class ShowController extends BaseAdmin
 {
 
@@ -11,12 +13,81 @@ class ShowController extends BaseAdmin
 
         $this->createTableData();
 
-        $this->createData();
+        $this->createData(['fields' => 'content']);
 
         return $this->expansion(get_defined_vars());
     }
 
     protected function outputData()
     {
+    }
+
+    protected function createData($arr = [])
+    {
+
+        $fields = [];
+        $order = [];
+        $order_direction = [];
+
+        if (!$this->columns['id_row']) return $this->data = [];
+
+        $fields[] = $this->columns['id_row'] . ' as id';
+        if ($this->columns['name']) $fields['name'] = 'name';
+        if ($this->columns['img']) $fields['img'] = 'img';
+
+        if (count($fields) < 3) {
+            foreach ($this->columns as $key => $item) {
+                if (!$fields['name'] && strpos($key, 'name') !== false) {
+                    $fields['name'] = $key . ' as name';
+                }
+                if (!$fields['img'] && strpos($key, 'img') == 0) {
+                    $fields['img'] = $key . ' as img';
+                }
+            }
+        }
+
+        if ($arr['fields']) {
+            if (is_array($arr['fields'])) {
+                $fields = Settings::instance()->arrayMergeRecursive($fields, $arr['fields']);
+            } else {
+                $fields[] = $arr['fields'];
+            }
+        }
+
+        if ($this->columns['parent_id']) {
+            if (!in_array('parent_id', $fields)) $fields[] = 'parent_id';
+            $order[] = 'parent_id';
+        }
+
+        if ($this->columns['manu_position']) $order[] = 'menu_position';
+        elseif ($this->columns['date']) {
+
+            if ($order) $order_direction = ['ASC', 'DESC'];
+            else $order_direction[] = 'DESC';
+
+            $order[] = 'date';
+        }
+
+        if ($arr['order']) {
+            if (is_array($arr['order'])) {
+                $order = Settings::instance()->arrayMergeRecursive($order, $arr['order']);
+            } else {
+                $order[] = $arr['order'];
+            }
+        }
+        if ($arr['order_direction']) {
+            if (is_array($arr['order_direction'])) {
+                $order_direction = Settings::instance()->arrayMergeRecursive($order_direction, $arr['order_direction']);
+            } else {
+                $order_direction[] = $arr['order_direction'];
+            }
+            $order_direction = Settings::instance()->arrayMergeRecursive($order_direction, $arr['order_direction']);
+        }
+
+        $this->data = $this->model->get($this->table, [
+            'fields' => $fields,
+            'order' => $order,
+            'order_direction' => $order_direction
+        ]);
     }
 }
